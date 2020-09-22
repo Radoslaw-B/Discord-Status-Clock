ee58cee9ce62959ea9e2bac5cc94570ad3782882
ThomasOreo <Thomas281204@gmail.com>
2020-09-22 23:14:07 +1000

    Update discord.py
    Fixed the emoji, idk what discord did

--- discord.py
+++ discord.py
@@ -3,14 +3,14 @@ from time import gmtime, strftime, localtime
 
 while True:
     if strftime("%S") == "00":
-        payload = '{"custom_status":{"text":"' + strftime("%I:%M %p | %y-%m-%d ⏰", localtime()) + '","emoji_name":"⏰"}}'
+        payload = '{"custom_status":{"text":"' + strftime("⏰ %I:%M %p | %y-%m-%d ⏰", localtime()) + '","emoji_name":""}}'
         requests.patch("https://discordapp.com/api/v6/users/@me/settings", payload.encode("utf-8",),
             headers={
                 "accept": "*/*",
                 "accept-encoding": "gzip, deflate, br",
                 "accept-language": "en-GB",
                 "authority": "discordapp.com",
-                "authorization": "!YOUR AUTH KEY HERE!",
+                "authorization": "!YOUR AUTH TOKEN HERE!",
                 "content-type": "application/json",
                 "origin": "https://discordapp.com",
                 "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.9 Chrome/69.0.3497.128 Electron/4.0.8 Safari/537.36",
